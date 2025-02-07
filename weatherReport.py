2fc81961473b07e9962737f2332e4d95dfab8709
Sarthakjain714 <jainsarthak6021@gmail.com>
2025-02-08 02:46:45 +0530

    adding 2 new endpoints

--- weatherReport.py
+++ weatherReport.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 app=Flask("WeatherReposrt")
 
+tabledata=pd.read_csv("data_small/stations.txt",skiprows=17)
+tabledata=tabledata[["STAID","STANAME                                 "]]
+
+
 @app.route("/")
 def home():
-    return render_template("weatherReportTempelate.html")
+    return render_template("weatherReportTempelate.html",data=tabledata.to_html())
 
 @app.route("/api1/v2/<station>/<date>")
 def info(station,date):
@@ -19,6 +23,24 @@ def info(station,date):
         "date" :date,
         "temperature" :temperature
     }
+
+@app.route("/api1/v2/<station>/")
+def Historicaldata(station):
+    filepath = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+
+    return df.to_dict(orient="records")
+
+@app.route("/api1/v2/yearly/<station>/<year>/")
+def yearlydata(station,year):
+    filepath = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20)
+    # print("here")
+    # print(df.head(10))
+    df["    DATE"]=df["    DATE"].astype(str)
+    result=df.loc[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 
 if __name__ == "__main__":
     app.run(debug=True)
